cd25be17ef68b0f2c2d8f09b324d2d0906efcd05
Sagar-Gopalasetti <gopalasettisagar@gmail.com>
2024-09-19 15:16:19 -0500

    padding for about

--- my-project/src/components/About.jsx
+++ my-project/src/components/About.jsx
@@ -1,16 +1,17 @@
-import {ABOUT_TEXT} from "../constants"
+import { ABOUT_TEXT } from "../constants";
 
 export const About = () => {
-  return <div className="borded-b border-neutral-900 pb-4">
-    <h2 className="my-10 text-center text-4xl">About
-    <span className="text-neutral-400"> Me</span>
-    </h2>
-    <div className="flex flex-wrap ">
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h2 className="my-10 text-center text-4xl">
+        About <span className="text-neutral-400">Me</span>
+      </h2>
+      <div className="flex flex-wrap">
         {/* <div className="w-full lg:w-1/2"></div> */}
-            <div className="flex items-center justify-center">
-            <p>{ABOUT_TEXT}</p>
-            </div>
-
+        <div className="flex items-center justify-center w-full">
+          <p className="px-6 lg:px-40">{ABOUT_TEXT}</p> {/* Added padding */}
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
